da5db0831923598e82b4238229688ad1b5d20cc3
Shehzad <shehzaddiqbal@gmail.com>
2022-11-11 23:56:26 -0800

    router addd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {isLogin ? null : (
+        <ul>
+          <li>
+            <Link to={`/`}>Login</Link>
+          </li>
+          <li>
+            <Link to={`/signup`}>Signup</Link>
+          </li>
+        </ul>
+      )}
+
+      {isLogin ? (
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+             {/* <Home/> */}
+              </>
+            }
+          />
+
+          {/* to remove */}
+          {/* <Route path="about" element={<About />} /> */}
+          {/* <Route path="profile" element={<Profile />} /> */}
+          {/* to remove */}
+
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="signup" element={<Signup />} /> */}
+          <Route path="*" element={<Navigate to="/login" replace={true} />} />
+        </Routes>
+      )}
     </div>
   );
 }
